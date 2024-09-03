b3ec585470b06aaee47bb36b70d468d4da7fcd60
Tuysh <angelmacloviomorales@gmail.com>
2024-09-03 10:16:28 -0600

    [Add] Carpeta creada para las funciones
    En esta carpeta van a ir las funciones de la pagina.

---
+++ src/lib/functions/hola.js
@@ -0,0 +1,18 @@
+/**
+ * 🐄 ¡Bienvenidos a la función más emocionante del universo! 🐄
+ * 
+ * ¿Alguna vez te has sentido solo y desamparado, buscando una forma de saludar a alguien en tu vida? 
+ * No busques más, porque aquí está la solución definitiva: ¡La función 'hola'! 🤯
+ * 
+ * @function hola
+ * @param {string} name - 🐄 El nombre de la víctima… eh, digo, la persona a la que quieres saludar. 😏
+ * @returns {string} - 🐄 Un increíble saludo con el nombre incluido, ¡como un regalo sorpresa! 🎁
+ * 
+ * @example
+ * // ¡Prepárate para ser sorprendido!
+ * hola('Mundo'); // 🐄 "Hola Mundo" 🐄
+ */
+export default function hola(name) {
+	// 🐄 Creamos el saludo más original jamás visto... "Hola", ¡sin creatividad pero efectivo! 🐄
+	return `Hola ${name}`	// 🐄 ¡Qué sorpresa! El resultado es un saludo. 😱
+}
