c59466c12750b8a0f9fb846f05a1f56be7583f87
Tuysh <angelmacloviomorales@gmail.com>
2024-09-03 10:20:47 -0600

    [Add] Carpeta data creada con un ejemplo
    En esta carpeta se contendra todo la información de la api, información y cosas así...

---
+++ src/lib/data/data.js
@@ -0,0 +1,17 @@
+/**
+ * 🐄 Función mística para obtener vacas. Porque, ¿quién no necesita más vacas en su vida? 🐄
+ * 
+ * Esta función es todo lo que siempre quisiste y más... ¡porque te devuelve vacas! 🐄🐄🐄
+ * No una, no dos, sino ¡tres gloriosas vacas! 🥳
+ *
+ * @function obtenerVacas
+ * @returns {Array<string>} - 🐄 Un increíble arreglo con tres vacas. Sí, tres. 🐄🐄🐄
+ * 
+ * @example
+ * // ¡Prepárate para la magia de las vacas!
+ * obtenerVacas(); // ['🐄', '🐄', '🐄'] 🐄 ¡Triple diversión!
+ */
+export default function obtenerVacas() {
+	// 🐄 Solo lo esencial: ¡tres vacas mágicas listas para alegrar tu día! 🐄
+	return ['🐄', '🐄', '🐄']	// 🐄 ¿Por qué detenerse en una cuando puedes tener tres? 🎉
+}
