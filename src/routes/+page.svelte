ab4e42e21c9e6222b721c04509ccd83f353cbc94
Tuysh <angelmacloviomorales@gmail.com>
2024-09-03 10:43:25 -0600

    [Add] Estructura de los componentes añadido
    Se añadio la estructura de los componentes y un pequeño ejemplo de como deben de ser los componentes.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+  import { BotonEspecial } from "$lib";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<BotonEspecial></BotonEspecial>
