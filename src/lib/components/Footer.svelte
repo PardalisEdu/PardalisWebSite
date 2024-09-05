81890473c98d38d85d89ddfdc34d3830b0d5116d
Tuysh <angelmacloviomorales@gmail.com>
2024-09-04 19:31:46 -0600

    [Add] Footer añadido

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,21 @@
+<footer class="bg-secundary rounded-lg shadow m-2 dark:bg-gray-800">
+	<div
+		class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"
+	>
+		<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+			>© 2024 <a href="/" class="hover:underline">Pardalis™</a>
+		</span>
+		<ul
+			class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
+		>
+			<li>
+				<a href="/about" class="hover:underline me-4 md:me-6">Sobre Nosotros</a>
+			</li>
+			<li>
+				<a href="/terms" class="hover:underline me-4 md:me-6"
+					>Términos y condiciones</a
+				>
+			</li>
+		</ul>
+	</div>
+</footer>
