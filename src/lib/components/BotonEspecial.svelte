3eb53bbea5855ad97ca8dc951dc5ec58afb6adcb
Tuysh <angelmacloviomorales@gmail.com>
2024-09-04 19:30:07 -0600

    [Format] Formateo del documento

--- src/lib/components/BotonEspecial.svelte
+++ src/lib/components/BotonEspecial.svelte
@@ -1,16 +1,16 @@
 <script>
-  // 🐄 Contador épico de muus... ¡porque necesitamos saber cuántos muus has hecho en tu vida! 🐄
-  let count = 0;
+	// 🐄 Contador épico de muus... ¡porque necesitamos saber cuántos muus has hecho en tu vida! 🐄
+	let count = 0;
 
-  /**
-   * 🐄 Función legendaria que hace que tu navegador diga "MuuUuu" y aumente el contador de muus. 🐄
-   *
-   * Esta función es tan poderosa que no podrás resistirte a hacer "MuuUuu" una y otra vez. 🐄
-   */
-  function moo() {
-    alert("MuuUuu"); // 🐄 La llamada del vaquero internauta. ¡Advertencia: puede ser adictivo! 😂
-    count++; // 🐄 Aumenta el contador de muus... ¡porque siempre se puede tener más muus! 🐄
-  }
+	/**
+	 * 🐄 Función legendaria que hace que tu navegador diga "MuuUuu" y aumente el contador de muus. 🐄
+	 *
+	 * Esta función es tan poderosa que no podrás resistirte a hacer "MuuUuu" una y otra vez. 🐄
+	 */
+	function moo() {
+		alert("MuuUuu"); // 🐄 La llamada del vaquero internauta. ¡Advertencia: puede ser adictivo! 😂
+		count++; // 🐄 Aumenta el contador de muus... ¡porque siempre se puede tener más muus! 🐄
+	}
 </script>
 
 <!-- 🐄 El botón más importante del universo. Haz clic y siente la magia del "MuuUuu". -->
--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,44 +1,44 @@
 <nav>
-  <div
-    class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
-  >
-    <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="" alt="Logo Pardalis" width="50" height="50" />
+	<div
+		class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+	>
+		<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+			<img src="" alt="Logo Pardalis" width="50" height="50" />
 
-      <span class="self-center text-2xl font-semibold whitespace-nowrap">
-        Pardalis
-      </span>
-    </a>
+			<span class="self-center text-2xl font-semibold whitespace-nowrap">
+				Pardalis
+			</span>
+		</a>
 
-    <div class="hidden w-full md:block md:w-auto">
-      <ul
-        class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0"
-      >
-        <li>
-          <a
-            href="/"
-            class="block py-2 px-3 rounded hover:text-primary md:hover:bg-transparent md:border-0 md:p-0"
-          >
-            Inicio
-          </a>
-        </li>
-        <li>
-          <a
-            href="/adventure"
-            class="block py-2 px-3 rounded hover:text-primary md:hover:bg-transparent md:border-0 md:p-0"
-          >
-            Aventura
-          </a>
-        </li>
-        <li>
-          <a
-            href="/profile"
-            class="block py-2 px-3 rounded hover:text-primary md:hover:bg-transparent md:border-0 md:p-0"
-          >
-            Perfil
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
+		<div class="hidden w-full md:block md:w-auto">
+			<ul
+				class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0"
+			>
+				<li>
+					<a
+						href="/"
+						class="block py-2 px-3 rounded hover:text-primary md:hover:bg-transparent md:border-0 md:p-0"
+					>
+						Inicio
+					</a>
+				</li>
+				<li>
+					<a
+						href="/adventure"
+						class="block py-2 px-3 rounded hover:text-primary md:hover:bg-transparent md:border-0 md:p-0"
+					>
+						Aventura
+					</a>
+				</li>
+				<li>
+					<a
+						href="/profile"
+						class="block py-2 px-3 rounded hover:text-primary md:hover:bg-transparent md:border-0 md:p-0"
+					>
+						Perfil
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
 </nav>
