ca069ad8be695cca6909d79b5d1085dbb178ee07
tuysh <angelmacloviomorales@gmail.com>
2026-06-13 15:06:37 -0600

    feat: limpiar proyecto

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    import { authStore } from '$lib/stores/authStore';
     import "../app.css";
     import NavBar from "$components/NavBar.svelte";
     import Footer from "$components/Footer.svelte";
 
-    let { data, children } = $props();
-
-    // Sync auth store from server-provided data on every navigation
-    $effect(() => {
-        authStore.sync(data.user ?? null);
-    });
+    let { children } = $props();
 
     const canonicalUrl = `https://pardalis.mx${$page.url.pathname}`;
 </script>
