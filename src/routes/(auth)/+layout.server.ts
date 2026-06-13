123f9505ec9220ff637f94bae820e0b7d9b25d96
tuysh <angelmacloviomorales@gmail.com>
2026-06-13 15:17:20 -0600

    feat: añadir comprobación
    Se añadio la comprobación para evitar que inicie sesión otra vez

---
+++ src/routes/(auth)/+layout.server.ts
@@ -0,0 +1,13 @@
+import { auth } from "$lib/server/auth";
+import { redirect } from "@sveltejs/kit";
+import type { LayoutServerLoad } from "./$types";
+
+export const load: LayoutServerLoad = async ({ request }) => {
+    const session = await auth.api.getSession({
+        headers: request.headers
+    });
+
+    if (session) {
+        throw redirect(302, '/profile');
+    }
+}
